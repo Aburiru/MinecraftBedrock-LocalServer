8950a9b62132c10e9531a75761e969c89306b567
Aburiru <jibrilliangilangs@gmail.com>
2026-04-09 09:32:29 +0800

    Modified

--- MCSeries.bat
+++ MCSeries.bat
@@ -1,129 +1,153 @@
 @echo off
-cls
-goto play
+setlocal EnableDelayedExpansion
 
-:play
-cls
-Title Minecraft Bedrock Dedicated Server - Menu
-goto menu
+:: ==========================================
+:: Configuration Section
+:: ==========================================
+set "TITLE=Minecraft Bedrock Dedicated Server - Menu"
+set "PLAYIT_EXE=C:\Program Files\playit_gg\bin\playit.exe"
+set "PLAYIT_PROCESS=playit.exe"
+set "PLAYIT_URL=https://playit.gg/account/tunnels"
 
+set "SERVER_DIR=C:\Users\abril\Documents\VibeCoding\MCServer\NewLife_1.26.13.1"
+set "SERVER_EXE=bedrock_server.exe"
+set "SERVER_PATH=%SERVER_DIR%\%SERVER_EXE%"
+
+:: ==========================================
+:: Elevation Check
+:: ==========================================
+:check_Permissions
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :init
+) else (
+    echo.
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:init
+title %TITLE%
+cls
+
+:: ==========================================
+:: Main Menu
+:: ==========================================
 :menu
 cls
-echo =======================
-echo    BDS Menu by abril
-echo =======================
-goto menu2
-
-:menu2
-echo.
-echo Bedrock Dedicated Server Menu.
+echo ==========================================
+echo       BDS Menu - Improved by Antigravity
+echo ==========================================
 echo.
 echo Notice:
-echo To turn activate Bedrock Dedicated Server, please activate the following requests.
-echo [1] Tunnel
-echo [2] BDS
-echo [3] VC
+echo This script will activate following services:
+echo [1] Tunnel (playit.gg)
+echo [2] Minecraft Bedrock Dedicated Server (BDS)
 echo.
-echo In a few moments the preparations will begin. Do you want to continue the process? (Y/N) 
+echo Do you want to continue the process? (Y/N)
 set /p "q= "
-if /I "%q%" == "Y" goto start
-if /I "%q%" == "N" goto menu2
+if /I "%q%" == "N" exit /b
+if /I "%q%" NEQ "Y" goto menu
 
 :start
 cls
-start "" "C:\Program Files\playit_gg\bin\playit.exe"
-start "" "https://playit.gg/account/tunnels"
-timeout /t 2 >nul
-:CEK
-tasklist /FI "IMAGENAME playit.exe" | find /I "playit.exe" >nul
-if %errorlevel%==0 (
-    echo playit.exe is not running. Please retry!
-    echo.
-    goto CEK
-) else (
-    echo playit.exe is running.
+echo [1/2] Starting playit.gg...
+
+:: Check if playit exists
+if not exist "%PLAYIT_EXE%" (
+    echo ALERT: playit.exe not found at "%PLAYIT_EXE%"
+    pause
+    goto menu
 )
 
+:: Start playit
+start "" "%PLAYIT_EXE%"
+start "" "%PLAYIT_URL%"
+timeout /t 2 >nul
+
+:cek_playit
+tasklist /FI "IMAGENAME eq %PLAYIT_PROCESS%" | find /I "%PLAYIT_PROCESS%" >nul
+if %errorlevel% neq 0 (
+    echo playit.exe is not running. Retrying...
+    timeout /t 2 >nul
+    goto cek_playit
+)
+echo playit.exe is running.
+
 echo.
-echo Activating the "Minecraft Bedrock Dedicated Server"..
-echo.
-:: Create a temporary script to run the server as administrator
+echo [2/2] Starting Bedrock Dedicated Server...
+
+:: Check if server exists
+if not exist "%SERVER_PATH%" (
+    echo ALERT: bedrock_server.exe not found at "%SERVER_PATH%"
+    pause
+    goto menu
+)
+
+:: Start server as a separate process
+:: We use a temporary script to ensure it runs correctly in its own window
 echo @echo off > "%temp%\run_bds.bat"
-echo start "" "C:\Users\abril\Documents\MCServer\World1\bedrock_server.exe" >> "%temp%\run_bds.bat"
+echo cd /d "%SERVER_DIR%" >> "%temp%\run_bds.bat"
+echo start "%SERVER_EXE%" "%SERVER_PATH%" >> "%temp%\run_bds.bat"
 echo exit >> "%temp%\run_bds.bat"
 
-:: Run the temporary script as administrator
-powershell -Command "Start-Process cmd -ArgumentList '/c %temp%\run_bds.bat' -Verb RunAs"
-timeout /t 2 >nul
+start "" "%temp%\run_bds.bat"
+timeout /t 3 >nul
 
-:CEK2
-tasklist /FI "IMAGENAME eq bedrock_server.exe" | find /I "bedrock_server.exe" >nul
-if %errorlevel%==0 (
-    echo bedrock_server.exe is running.
-) else (
-    echo bedrock_server.exe is not running. Please retry!
-    goto CEK2
+:cek_server
+tasklist /FI "IMAGENAME eq %SERVER_EXE%" | find /I "%SERVER_EXE%" >nul
+if %errorlevel% neq 0 (
+    echo bedrock_server.exe is not running. Retrying...
+    timeout /t 2 >nul
+    goto cek_server
 )
+echo bedrock_server.exe is running.
 
-@REM echo Activating the "Proximity Voice Chat"..
-@REM echo.
-@REM :: Create a temporary script to run the VC as administrator
-@REM echo @echo off > "%temp%\run_vc.bat"
-@REM echo start "" "D:\Abril\Application\minecraft\VoiceCraft.Server\VoiceCraft.Server.exe" >> "%temp%\run_vc.bat"
-@REM echo exit >> "%temp%\run_vc.bat"
-
-@REM :: Run the temporary script as administrator
-@REM powershell -Command "Start-Process cmd -ArgumentList '/c %temp%\run_vc.bat' -Verb RunAs"
-@REM timeout /t 2 >nul
-
-@REM :CEK3
-@REM tasklist /FI "IMAGENAME eq VoiceCraft.Server.exe" | find /I "VoiceCraft.Server.exe" >nul
-@REM if %errorlevel%==0 (
-@REM     echo VoiceCraft.Server.exe is running.
-@REM     echo.
-@REM     echo Follow the next command to complete VC mode.
-@REM     echo Enter the following IP address and Port.
-@REM     echo IP  : 127.0.0.1
-@REM     echo PORT: 9050
-@REM     echo Use the following command to enable VC mode. 
-@REM     echo "/scriptevent voicecraft:voice connect [IP] [Port]"
-@REM ) else (
-@REM     echo VoiceCraft.Server.exe is not running. Please retry!
-@REM     goto CEK3
-@REM )
-
-:menu3
+:: ==========================================
+:: Status Menu
+:: ==========================================
+:status
 cls
-echo =======================
-echo    BDS Menu by abril
-echo =======================
+echo ==========================================
+echo       BDS Server Status: ACTIVATED
+echo ==========================================
 echo.
-echo Bedrock Dedicated Server Status: Activated
-echo Tunnel: Online
-echo BDS   : Online
-echo VC    : Online
+echo Tunnel: ONLINE
+echo BDS   : ONLINE
 echo.
+echo ------------------------------------------
+echo Options:
+echo [Exit]    Terminate all and close
+echo [Restart] Restart the activation process
 echo.
-pause
-echo [Exit] to exit
-echo [Restart] to restart
-set /p "q1= "
+set /p "q1=Selection: "
 if /I "%q1%" == "exit" goto exit
-if /I "%q1%" == "restart" goto start
+if /I "%q1%" == "restart" (
+    echo Restarting...
+    taskkill /F /T /IM "%SERVER_EXE%" >nul 2>&1
+    taskkill /F /T /IM "%PLAYIT_PROCESS%" >nul 2>&1
+    taskkill /F /T /IM "playit-windows-x86_64-signed.exe" >nul 2>&1
+    timeout /t 1 >nul
+    start "" "%~f0"
+    exit /b
+)
+goto status
 
 :exit
+cls
 echo.
-echo Exiting the script and closing all related applications...
+echo Terminating running applications...
 echo.
 
-:: Terminate the running applications gracefully
-taskkill /F /T /IM "bedrock_server.exe" >nul 2>&1
-timeout /t 2 >nul
-taskkill /F /T /IM "VoiceCraft.Server.exe" >nul 2>&1
-timeout /t 2 >nul
+:: Taskkill with forceful termination
+taskkill /F /T /IM "%SERVER_EXE%" >nul 2>&1
+taskkill /F /T /IM "%PLAYIT_PROCESS%" >nul 2>&1
+:: Also kill the signed version seen in original code
 taskkill /F /T /IM "playit-windows-x86_64-signed.exe" >nul 2>&1
-timeout /t 2 >nul
 
+timeout /t 2 >nul
+echo.
 echo All applications have been terminated.
 pause
 exit /b
